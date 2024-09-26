40c6f0bf409e8885d5e4004398ab1cdc9a0011b8
Expedit206 <mciagnessi@gmail.com>
2024-09-26 12:02:29 +0100

    ..

--- resources/views/note/assign.blade.php
+++ resources/views/note/assign.blade.php
@@ -8,140 +8,91 @@
         {{-- @dd($uniteValeurs) --}}
 
         <div class="filter">
-            <div class="flex justify-between ">
-                <a href="{{ route('uniteValeur.create') }}" class="btn text-violet-800 font-bold flex w-1/3">Ajouter une unité
-                    de valeur
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24px"
-                        height="24px">
-                        <path
-                            d="M10.293 6.293L8.879 7.707 13.172 12 8.879 16.293l1.414 1.414L16 12zM14.293 6.293L12.879 7.707 17.172 12 12.879 16.293l1.414 1.414L20 12z" />
+            <div class="flex justify-between">
+                <a href="{{ route('uniteValeur.create') }}" class="btn text-violet-800 font-bold flex w-1/3">
+                    Ajouter une unité de valeur
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24px" height="24px">
+                        <path d="M10.293 6.293L8.879 7.707 13.172 12 8.879 16.293l1.414 1.414L16 12zM14.293 6.293L12.879 7.707 17.172 12 12.879 16.293l1.414 1.414L20 12z" />
                     </svg>
                 </a>
+
                 <div class="font-bold">
-                    <form method="get" action="{{ route('uniteValeur.index') }}" id="form"
-                        class="px-3 text-white grid-cols-5 content flex gap-3 items-center justify-around bg-orange-400 py-2">
+                    <form method='post' action="{{ route('annee.setActive') }}" id="formAnnee">
                         @csrf
-
-                        <h3>Filtrer par:</h3>
-
-                        <article class="flex flex-col w-full">
-                            <label for="niveau">Niveau</label>
-                            <select id="niveau" name="niveau" class="text-black rounded-md w-full" onchange="submit()">
-                                <option value=""></option>
-                                @foreach ($niveaux as $niveau)
-                                    <option value="{{ $niveau->nom }}"
-                                        {{ request('niveau') === $niveau->nom ? 'selected' : '' }}>
-                                        {{ $niveau->nom }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </article>
-
-                        <article class="flex flex-col w-full">
-                            <label for="specialite">Spécialité</label>
-                            <select id="specialite" name="specialite" class="text-black rounded-md w-full"
-                                onchange="submit()">
-                                <option value=""></option>
-                                @foreach ($specialites as $specialite)
-                                    <option value="{{ $specialite->nom }}"
-                                        {{ request('specialite') === $specialite->nom ? 'selected' : '' }}>
-                                        {{ $specialite->nom }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </article>
-
-                        <article class="flex flex-col w-full">
-                            <label for="semestre">Semestre</label>
-                            <select id="semestre" name="semestre" class="text-black rounded-md w-full" onchange="submit()">
-                                <option value=""></option>
-                                @foreach ($semestres as $semestre)
-                                    <option value="{{ $semestre->nom }}"
-                                        {{ request('semestre') === $semestre->nom ? 'selected' : '' }}>
-                                        {{ $semestre->nom }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </article>
-
-                        <article class="flex flex-col w-full">
-                            <label for="unitevaleur">Unité de Valeur</label>
-                            <select id="unitevaleur" name="unitevaleur" class="text-black rounded-md w-full"
-                                onchange="submit()">
-                                <option value=""></option>
-                                @foreach ($uniteValeurs as $unitevaleur)
-                                    <option value="{{ $unitevaleur->nom }}"
-                                        {{ request('unitevaleur') === $unitevaleur->nom ? 'selected' : '' }}>
-                                        {{ $unitevaleur->nom }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </article>
+                        <label for="annee">Année Académique</label>
+                        <select name="annee" id="annee" class="border-none outline-none focus:border-none cursor-pointer"
+                            onchange="submit()">
+                            @foreach ($annees as $annee)
+                                <option value="{{ $annee->id }}"
+                                    class="cursor-pointer border-b-4 border-double border-black"
+                                    {{ $annee->is_active == true ? 'selected' : '' }}>
+                                    {{ $annee->nom }}
+                                </option>
+                            @endforeach
+                        </select>
                     </form>
-
                 </div>
             </div>
+
+            <!-- Filtre par paramètres -->
             <form method="get" action="{{ route('uniteValeur.index') }}" id="form"
                 class="px-3 text-white grid-cols-5 content flex gap-3 items-center justify-around bg-orange-400 py-2">
                 @csrf
-
 
                 <h3>Filtrer par:</h3>
 
                 <article class="flex flex-col w-full">
                     <label for="niveau">Niveau</label>
-                    <select type="text" id="niveau" name="niveau" class="text-black rounded-md w-full"
-                        onchange="submit()">
+                    <select id="niveau" name="niveau" class="text-black rounded-md w-full" onchange="submit()">
                         <option value=""></option>
                         @foreach ($niveaux as $niveau)
-                            <option value="{{ $niveau->nom }}"
-                                {{ request('niveau') === $niveau->nom ? 'selected' : '' }}>
-                                {{ $niveau->nom }}</option>
-                        @endforeach
-                    </select>
-                </article>
-
-
-
-                <article class="flex flex-col w-full">
-                    <label for="filiere">spécialité</label>
-                    <select type="text" id="specialite" name="specialite" class="text-black rounded-md w-full"
-                        onchange="submit()">
-                        <option value=""></option>
-                        @foreach ($specialites as $specialite)
-                            <option value="{{ $specialite->nom }}"
-                                {{ request('specialite') === $specialite->nom ? 'selected' : '' }}>
-                                {{ $specialite->nom }}</option>
-                        @endforeach
-                    </select>
-                </article>
-                <article class="flex flex-col w-full">
-                    <label for="semestre">semestres</label>
-                    <select type="text" id="semestre" name="semestre" class="text-black rounded-md w-full"
-                        onchange="submit()">
-                        <option value=""></option>
-                        @foreach ($semestres as $semestre)
-                            <option value="{{ $semestre->nom }}"
-                                {{ request('semestre') === $semestre->nom ? 'selected' : '' }}>{{ $semestre->nom }}
+                            <option value="{{ $niveau->id }}" {{ request('niveau') == $niveau->id ? 'selected' : '' }}>
+                                {{ $niveau->nom }}
                             </option>
                         @endforeach
                     </select>
                 </article>
 
                 <article class="flex flex-col w-full">
+                    <label for="specialite">Spécialité</label>
+                    <select id="specialite" name="specialite" class="text-black rounded-md w-full" onchange="submit()">
+                        <option value=""></option>
+                        @foreach ($specialites as $specialite)
+                            <option value="{{ $specialite->id }}" {{ request('specialite') == $specialite->id ? 'selected' : '' }}>
+                                {{ $specialite->nom }}
+                            </option>
+                        @endforeach
+                    </select>
+                </article>
+
+                <article class="flex flex-col w-full">
+                    <label for="semestre">Semestre</label>
+                    <select id="semestre" name="semestre" class="text-black rounded-md w-full" onchange="submit()">
+                        <option value=""></option>
+                        @foreach ($semestres as $semestre)
+                            <option value="{{ $semestre->id }}" {{ request('semestre') == $semestre->id ? 'selected' : '' }}>
+                                {{ $semestre->nom }}
+                            </option>
+                        @endforeach
+                    </select>
+                </article>
+
+
+
+                <article class="flex flex-col w-full">
                     <label for="unitevaleur">Unité de Valeur</label>
-                    <select type="text" id="unitevaleur" name="unitevaleur" class="text-black rounded-md w-full"
-                        onchange="submit()">
+                    <select id="unitevaleur" name="unitevaleur" class="text-black rounded-md w-full" onchange="submit()">
                         <option value=""></option>
                         @foreach ($uniteValeurs as $unitevaleur)
-                            <option value="{{ $unitevaleur->nom }}"
-                                {{ request('unitevaleur') === $unitevaleur->nom ? 'selected' : '' }}>
-                                {{ $unitevaleur->nom }}</option>
+                            <option value="{{ $unitevaleur->id }}" {{ request('matieres') == $unitevaleur->id ? 'selected' : '' }}>
+                                {{ $unitevaleur->nom }}
+                            </option>
                         @endforeach
                     </select>
                 </article>
             </form>
         </div>
+
 
         <div class="table mt-3">
             {{-- <p class="font-bold text-1xl italic">Total : {{ $total }}</p> --}}
